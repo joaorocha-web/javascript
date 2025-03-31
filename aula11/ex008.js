2f26d67ed9f0ef595bd653447a82aea8598244a7
joaorocha-web <jvrochajf@eaportal.org>
2025-03-31 12:07:50 -0300

    Exercícios 8 ao 11 de javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+if (vel > 60){
+    console.log (`O seu carro ultrapassou o limite de velocidade. MULTADO!`)
+} else{
+    console.log (` A velocidade do seu automóvel é de ${vel}km por hora`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+    console.log ('Brasileiro não desite nunca')
+    
+}
+    else{
+        console.log ('Gringo não é bom de bola')
+    }
